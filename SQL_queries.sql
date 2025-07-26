c14ba8564ea78fb8b65ade3f0fe5d2a980b9f975
Anushka Chauhan <164030867+anushkachauhan357@users.noreply.github.com>
2025-07-26 19:58:23 +0530

    Create SQL_queries.sql

---
+++ SQL_queries.sql
@@ -0,0 +1,65 @@
+-- Query 1: Total Sales and Total Profit
+SELECT 
+    ROUND(SUM(Sales), 2) AS Total_Sales,
+    ROUND(SUM(Profit), 2) AS Total_Profit
+FROM sales;
+
+-- Query 2: Customer sSegmentation by Total Spending
+SELECT 
+    CustomerName,
+    ROUND(SUM(Sales), 2) AS Total_Spent
+FROM sales
+GROUP BY CustomerName
+ORDER BY Total_Spent DESC
+LIMIT 10;
+
+-- Query 3: Monthly Sales Trend
+SELECT 
+    DATE_FORMAT(OrderDate, '%Y-%m') AS Month,
+    ROUND(SUM(Sales), 2) AS Monthly_Sales
+FROM sales
+GROUP BY DATE_FORMAT(OrderDate, '%Y-%m')
+ORDER BY Month;
+
+-- Query 4: Top 5 Selling Products
+SELECT 
+    ProductName,
+    SUM(Quantity) AS Total_Units_Sold
+FROM sales
+GROUP BY ProductName
+ORDER BY Total_Units_Sold DESC
+LIMIT 5;
+
+-- Query 5: Category and Sub-Category Sales Breakdown
+SELECT 
+    Category,
+    SubCategory,
+    ROUND(SUM(Sales), 2) AS Revenue
+FROM sales
+GROUP BY Category, SubCategory
+ORDER BY Revenue DESC;
+
+-- Query 6: Profit Margin by Category
+SELECT 
+    Category,
+    ROUND(SUM(Profit), 2) AS Total_Profit,
+    ROUND(SUM(Profit) / SUM(Sales) * 100, 2) AS Profit_Margin_Percentage
+FROM sales
+GROUP BY Category;
+
+-- Query 7: Region-wise Total Sales
+SELECT 
+    Region,
+    ROUND(SUM(Sales), 2) AS Total_Sales
+FROM sales
+GROUP BY Region
+ORDER BY Total_Sales DESC;
+
+-- Query 8: Discount Impact on Profit
+SELECT 
+    Discount,
+    ROUND(AVG(Profit), 2) AS Average_Profit
+FROM sales
+GROUP BY Discount
+ORDER BY Discount;
+
